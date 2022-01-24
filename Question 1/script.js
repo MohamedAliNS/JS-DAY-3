863c559dbac84bfd7fa42c7f2dfad47e87df2ef2
Mohamed Ali NS <mohamedali.elmac@gmail.com>
2022-01-24 20:51:11 +0530

    Question 1

--- Question 1/script.js
+++ Question 1/script.js
@@ -1,38 +1,20 @@
 // Question 1: For the given JSON iterate over all for loops (for, for in, for of, forEach)
 // Array used on JSON to execute using for loop
 // For in loop
-let a = [{
-    "name":"Raja Mohamed", 
-    "age":"25", 
-    "DOB":"15th January 1997",
-}];
-    for (let i in a) {
-        console.log(i,a[i]);
-    }
-//     // For loop
-let b = [{
-    "name":"Raja Mohamed", 
-    "age":"25", 
-    "DOB":"15th January 1997",
-}];
-for (let i=0; i<b.length; i++) {
+let a = ['{"name":"Mohamed", "age":25, "phone": 7001290887,}'];
+    for (let i in a) 
+     console.log(i,a[i]);
+      // For loop
+let b = ['{"name":"Raja", "age":28, "phone": 9840031444,}'];
+for (let i=0; i<b.length; i++) 
         console.log(b[i]);
-} 
-    // For each loop
-    let c = [{
-        "name":"Raja Mohamed", 
-        "age":"25", 
-        "DOB":"15th January 1997",
-    }];
+//     // For each loop
+    let c = ['{"name":"Ram", "age":26, "phone": 9840031555,}'];
 c.forEach (i => console.log(i));
-// For of loop
-let d = [{
-    "name":"Raja Mohamed", 
-    "age":"25", 
-    "DOB":"15th January 1997",
-}];
-for (let i of d) {
+// // For of loop
+let d = ['{"name":"Ravi", "age":27, "phone": 9840031420,}'];
+for (let i of d) 
     console.log(i);
-}
+
 
 
