71fb7d258ed4fb3f7b16cd93cd24f760bbb060ed
Mohamed Ali NS <mohamedali.elmac@gmail.com>
2022-01-24 22:29:36 +0530

    Question 2

---
+++ Question 2/script.js
@@ -0,0 +1,13 @@
+// Create your own resume data in JSON format
+let resume = {
+    "name": "Mohamed", 
+    "email": "mohamed@gmail.com",
+    "phone": 9840031555,
+    "address": {
+        "no": 1,
+        "street": "Saamy st, muthunagar",
+        "district": "chennai",
+        "state": "tamilnadu"
+        }
+};
+console.log(resume);
